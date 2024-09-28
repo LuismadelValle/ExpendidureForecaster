1ee6f94e0c9f29d15003935a65373df07ada1584
Luisma <40072055+LuismadelValle@users.noreply.github.com>
2024-09-28 15:06:32 -0400

    average for several months done

--- projectIdea.py
+++ projectIdea.py
@@ -1,11 +1,24 @@
 # import pandas as pd
 # from pandas import DataFrame
 
+expenditureList = []
 amountOfMonthsToAverage = int(input("Cuantos meses desea controlar? "))
-lst = list(map(int, input(f"Ingrese los gastos de los ultimos {amountOfMonthsToAverage} meses ").strip().split()))[:amountOfMonthsToAverage]
 
-averageExpenditure = sum(lst)/amountOfMonthsToAverage
+print(f"Desglose sus gastos en alquiler(1), facturas(2), comida(3) y otros(4)")
 
+currentMonth = 1
+for i in range(amountOfMonthsToAverage):
+  iRange = 1
+  tempList = []
+  for j in range (4):
+    element = int(input(f"Gastos en {iRange} para el mes {currentMonth} "))
+    iRange = iRange + 1
+    tempList.append(element)
+  currentMonth = currentMonth + 1
+  expenditureList.append(tempList)
+
+
+averageExpenditure = sum(sum(expenditureList, []))/amountOfMonthsToAverage
 # df = DataFrame({'Primer Mes': expenditure1, 'Segundo Mes': expenditure2, 'Promedio': averageExpenditure})
 
 # df.to_excel('test.xlsx', sheet_name='Testing', index=False) install pandas on monday
