69baaaef4fce6a95d6dd5a92a1904f4f4abb25db
Luisma <40072055+LuismadelValle@users.noreply.github.com>
2024-10-01 09:30:31 -0400

    adding first endpoints

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-  return {"Hello": "World"}
+  return {"Welcome Data"}
+
+@app.get("dashboardData/")
+async def getDashboardData(user_id: int):
+  return{"user_id": user_id}
 
 @app.get("items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
   return{"item_id": item_id, "q": q} 
 
+@app.get("yearData/{year_id}")
+async def read_year(year_id: int, user_id: int):
+  return{"year_id": year_id}
 
 #api doc at 127.0.0.1:8000/docs (swagger)
 
