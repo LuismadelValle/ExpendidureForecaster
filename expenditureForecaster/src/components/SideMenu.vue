019dab36219bba2b96a9b9887b406a5ce166f5fe
Luisma <luismadf95@gmail.com>
2024-12-21 14:48:31 +0100

    fixing imports for app.vue

--- expenditureForecaster/src/components/SideMenu.vue
+++ expenditureForecaster/src/components/SideMenu.vue
@@ -1,49 +1,47 @@
 <template>
-  <sidebar-menu :menu="menu" :collapsed="collapsed" :width="width"  @update:collapsed="onToggleCollapse" @item-click="onItemClick"/>
+  <sidebar-menu :menu="menu" :width="width" @update:collapsed="onToggleCollapse" @item-click="onItemClick" />
 </template>
 
 <script>
-// icons at https://primevue.org/icons/
-  import { SidebarMenu } from 'vue-sidebar-menu'
-  import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
-    
-  export default {    
-    components: {
-      SidebarMenu,
-    },
+import { SidebarMenu } from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+
+export default {
+	components: {
+    	SidebarMenu
+  	},
     data() {
       return {
-        collapsed: true,
-        width: '240px', 
         menu: [
           {
-            header: 'Menu',
-            hiddenOnCollapse: true,
-            external: false
+            header: 'User Menu',
+            hiddenOnCollapse: true
           },
-          {
-            title: 'User Name',
-            icon: 'pi pi-user',
-          },
+		  {
+			href: '/user',
+			title: 'Username',
+			icon: 'pi pi-user',
+		  },
           {
             href: '/',
             title: 'Dashboard',
-            icon: 'pi pi-objects-column',
+            icon: 'pi pi-objects-column'
           },
           {
+            href: '/budget',
             title: 'Budget',
             icon: 'pi pi-wallet',
             child: [
-              {
+			{
                 href: '/budget/personalFinances',
                 title: 'Personal Budget',
               }, {
                 href: '/budget/familyFinances',
                 title: 'Family Budget'
               }
-            ],
+            ]
           },
-          {
+		  {
             title: 'Forecaster',
             icon: 'pi pi-chart-scatter',
             child: [
@@ -60,51 +58,48 @@
           {
             href: '/settings',
             title: 'User Settings',
-            icon: 'pi pi-face-smile'
+            icon: 'pi pi-cog'
           }
-        ],
+        ]
       }
     },
-    methods: {
-      onToggleCollapse() {
-        this.collapsed = !this.collapsed
-        console.log(this.collapsed)
-      },
-      // onItemClick(event, item) {}
-    }
-  }
+	methods: {
+		onToggleCollapse(collapsed) {},
+		onItemClick(event, item) {}
+	}
+  }  
 </script>
 
-<style>
+<style scoped>
   .v-sidebar-menu {
-    --vsm-primary-color: #4285f4;
-    --vsm-base-bg: #2a2a2e;
-    --vsm-item-color: #fff;
-    --vsm-item-active-color:#FFFFFF;
-    --vsm-item-active-bg:#2a2a2e;
-    --vsm-item-active-line-color: var(--vsm-primary-color);
-    --vsm-item-open-color: #fff;
-    --vsm-item-hover-color:#fff;
-    --vsm-item-open-bg: var(--vsm-primary-color);
-    --vsm-item-hover-bg: rgba(30, 30, 33, 0.5);
-    --vsm-icon-color: var(--vsm-item-color);
-    --vsm-icon-bg: #1e1e21;
-    --vsm-icon-active-color: #fff;
-    --vsm-icon-active-bg:transparent;
-    --vsm-icon-open-color: #fff;
-    --vsm-icon-open-bg:transparent;
-    --vsm-mobile-item-color: #fff;
-    --vsm-mobile-item-bg: var(--vsm-primary-color);
-    --vsm-mobile-icon-color: var(--vsm-mobile-item-color);
-    --vsm-mobile-icon-bg: transparent;
-    --vsm-dropdown-bg: #36363b;
-    --vsm-header-item-color: rgba(255, 255, 255, 0.7);
-    --vsm-toggle-btn-color: #fff;
-    --vsm-toggle-btn-bg: #1e1e21;
-    --vsm-item-font-size: 16px;
-    --vsm-item-line-height: 35px;
-    --vsm-item-padding: 10px 15px;
-    --vsm-icon-height: 35px;
-    --vsm-icon-width: 35px;
+	--vsm-primary-color: #4285f4;
+	--vsm-base-bg: #2a2a2e;
+	--vsm-item-color: #fff;
+	--vsm-item-active-color:#FFFFFF;
+	--vsm-item-active-bg:#2a2a2e;
+	--vsm-item-active-line-color: var(--vsm-primary-color);
+	--vsm-item-open-color: #fff;
+	--vsm-item-hover-color:#fff;
+	--vsm-item-open-bg: var(--vsm-primary-color);
+	--vsm-item-hover-bg: rgba(30, 30, 33, 0.5);
+	--vsm-icon-color: var(--vsm-item-color);
+	--vsm-icon-bg: #1e1e21;
+	--vsm-icon-active-color: #fff;
+	--vsm-icon-active-bg:transparent;
+	--vsm-icon-open-color: #fff;
+	--vsm-icon-open-bg:transparent;
+	--vsm-mobile-item-color: #fff;
+	--vsm-mobile-item-bg: var(--vsm-primary-color);
+	--vsm-mobile-icon-color: var(--vsm-mobile-item-color);
+	--vsm-mobile-icon-bg: transparent;
+	--vsm-dropdown-bg: #36363b;
+	--vsm-header-item-color: rgba(255, 255, 255, 0.7);
+	--vsm-toggle-btn-color: #fff;
+	--vsm-toggle-btn-bg: #1e1e21;
+	--vsm-item-font-size: 16px;
+	--vsm-item-line-height: 35px;
+	--vsm-item-padding: 10px 15px;
+	--vsm-icon-height: 35px;
+	--vsm-icon-width: 35px;
   }
 </style>
--- expenditureForecaster/src/main.ts
+++ expenditureForecaster/src/main.ts
@@ -2,17 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import SideMenu from './components/SideMenu.vue'
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 const routes = [
   {
-    path: '/', component: SideMenu,
-    path2: '/budget/personalFinances', component2: SideMenu,
-    path3: '/budget/familyFinances', component3: SideMenu,
-    path4: '/forecaster/user', component4: SideMenu,
-    path5: '/forecaster/family', component5: SideMenu,
-    path6: '/settings', component6: SideMenu,
+    path: '/',
+    component: App,
+    // path2: '/budget/personalFinances', component2: SideMenu,
+    // path3: '/budget/familyFinances', component3: SideMenu,
+    // path4: '/forecaster/user', component4: SideMenu,
+    // path5: '/forecaster/family', component5: SideMenu,
+    // path6: '/settings', component6: SideMenu,
   },
 ]
 
