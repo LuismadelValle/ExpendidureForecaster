18fbca11f40203486c52b8e4e435bb2e90aa9381
Luisma <luismadf95@gmail.com>
2025-05-16 21:46:11 +0200

    Adding first endpoint and data is displaying at table

---
+++ README.md
@@ -0,0 +1,3 @@
+Use python -m fastapi run main.py for local development
+
+python -m uvicorn main:app --reload use for reloads (i guess)
--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-from typing import Union
-from fastapi import FastAPI
-# from projectIdea import *
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Optional, List, Dict
+from pydantic import BaseModel
+from uuid import UUID, uuid4
+from fastapi.middleware.cors import CORSMiddleware
+from dashboard import expenditureDistribution
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
   return {"Welcome Data"}
 
-@app.get("dashboardData/")
-async def getDashboardData(user_id: int):
-  return{"user_id": user_id}
+@app.get("/dashboard_last_personal_list")
+def get_expenditure_distribution():
+    return expenditureDistribution()
 
-@app.get("items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-  return{"item_id": item_id, "q": q} 
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#   return{"item_id": item_id, "q": q} 
 
-@app.get("yearData/{year_id}")
-async def read_year(year_id: int, user_id: int):
-  return{"year_id": year_id}
+# @app.get("/yearData/{year_id}")
+# async def read_year(year_id: int, user_id: int):
+#   return{"year_id": year_id}
 
 #api doc at 127.0.0.1:8000/docs (swagger)
 
