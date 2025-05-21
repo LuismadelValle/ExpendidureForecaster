b161edff2ba7d2c4cd3430ddfafd20437cccd886
Luisma <luismadf95@gmail.com>
2025-05-21 20:32:10 +0200

    fixing page not filling completely the screen issues

--- expenditureForecaster/src/App.vue
+++ expenditureForecaster/src/App.vue
@@ -8,3 +8,27 @@
 import 'primeicons/primeicons.css';
 import MainComponent from './components/mainComponent.vue';
 </script>
+
+<style scoped>
+html, body, #app, .v-application {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+}
+
+.v-main {
+  flex: 1;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.v-container {
+  max-width: 100% !important;
+  padding: 0 !important;
+}
+</style>
